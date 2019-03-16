a070434213474871af3678ca3a37052d2b626d46
eduardocalixto <eduardogubertcalixto@viasoft.com.br>
2019-03-16 11:17:50 -0300

    CS-1 Added Base layout components

--- frontend/apps/country-stats/src/app/app.module.ts
+++ frontend/apps/country-stats/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AccountAccessModule } from './account-access/account-access.module';
 import { AdminModule } from './admin/admin.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutModule } from './layout/layout.module';
 import { MainModule } from './main/main.module';
 
 
@@ -12,10 +15,13 @@ import { MainModule } from './main/main.module';
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
     MainModule,
     AdminModule,
-    AccountAccessModule
+    AccountAccessModule,
+    LayoutModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
