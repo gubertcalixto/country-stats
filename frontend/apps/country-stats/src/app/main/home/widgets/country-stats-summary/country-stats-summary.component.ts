c1249c54a1b9dac6f06ca918b40aa8a2d532f98d
eduardocalixto <eduardogubertcalixto@viasoft.com.br>
2019-03-24 21:29:37 -0300

    Added country stats summary component

---
+++ frontend/apps/country-stats/src/app/main/home/widgets/country-stats-summary/country-stats-summary.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+
+import { CsBase } from '../../../../shared/cs-base.component';
+
+@Component({
+  selector: 'cs-country-stats-summary',
+  templateUrl: './country-stats-summary.component.html',
+  styleUrls: ['./country-stats-summary.component.scss']
+})
+export class CountryStatsSummaryComponent extends CsBase {
+  populationData: any[];
+  costData: any[];
+  tourismData: any[];
+  populationColorScheme = { domain: ['#5AA454'] };
+  costColorScheme = { domain: ['#A10A28'] };
+  tourismColorScheme = { domain: ['#C7B42C'] };
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Country';
+  showYAxisLabel = true;
+  yAxisLabel = 'Population';
+
+  // line, area
+  autoScale = true;
+
+  onSelect(event) {
+    console.log(event);
+  }
+
+  constructor() {
+    super();
+    this.populationData = populationDataBase;
+    this.costData = costDataBase;
+    this.tourismData = tourismDataBase;
+  }
+
+}
+export const populationDataBase = [{ name: '8940000', value: 8940000 }];
+export const costDataBase = [{ name: '5000000', value: 5000000 }];
+export const tourismDataBase = [{ name: '7200000', value: 7200000 }];
--- frontend/apps/country-stats/src/app/main/home/widgets/widgets.module.ts
+++ frontend/apps/country-stats/src/app/main/home/widgets/widgets.module.ts
@@ -3,24 +3,35 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule, MatCheckboxModule, MatDividerModule, MatIconModule, MatTooltipModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatDividerModule,
+  MatIconModule,
+  MatTooltipModule,
+} from '@angular/material';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
+import { CountryStatsSummaryComponent } from './country-stats-summary/country-stats-summary.component';
 import { WidgetsSettingsComponent } from './widgets-settings/widgets-settings.component';
 
 @NgModule({
-  declarations: [WidgetsSettingsComponent],
+  declarations: [WidgetsSettingsComponent, CountryStatsSummaryComponent],
   imports: [
     CommonModule,
     MatIconModule,
     MatButtonModule,
+    MatCardModule,
     FormsModule,
     ReactiveFormsModule,
     MatCheckboxModule,
     FlexLayoutModule,
     MatDividerModule,
     MatTooltipModule,
-    DragDropModule
+    DragDropModule,
+    NgxChartsModule
   ],
-  exports: [WidgetsSettingsComponent]
+  exports: [WidgetsSettingsComponent, CountryStatsSummaryComponent]
 })
 export class WidgetsModule { }
