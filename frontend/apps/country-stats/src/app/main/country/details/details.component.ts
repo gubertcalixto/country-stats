4c5fc988d17be99ad64f6e709e1a0404255ae8df
eduardocalixto <eduardogubertcalixto@viasoft.com.br>
2019-04-14 00:49:58 -0300

    CS-8 Fixed details build prod error

--- frontend/apps/country-stats/src/app/main/country/details/details.component.ts
+++ frontend/apps/country-stats/src/app/main/country/details/details.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CsBase } from '../../../shared/cs-base.component';
+
 @Component({
   selector: 'cs-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent implements OnInit {
+export class DetailsComponent extends CsBase implements OnInit {
+  country: any;
   defaultBackgroundImage = 'apps/country-stats/src/assets/images/country - details.jpg';
-  constructor() { }
+
+  constructor() {
+    super();
+  }
 
   ngOnInit() {
   }
