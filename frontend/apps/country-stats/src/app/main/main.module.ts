f7270b946b6ca7b7e9101459592157a89b2797f9
eduardocalixto <eduardogubertcalixto@gmail.com>
2019-06-21 23:27:34 -0300

    Added counrty service

--- frontend/apps/country-stats/src/app/main/main.module.ts
+++ frontend/apps/country-stats/src/app/main/main.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
+import {CountryService} from "./country/country.service";
 
 @NgModule({
   declarations: [MainComponent],
   imports: [
     CommonModule,
     MainRoutingModule
+  ],
+  providers: [
+    CountryService
   ],
   exports: [MainComponent]
 })
