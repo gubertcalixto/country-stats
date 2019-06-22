05567912873e2b501a4b0576bd6de76f8dc84f51
eduardocalixto <eduardogubertcalixto@gmail.com>
2019-06-21 23:27:20 -0300

    Added localStorage vars

--- frontend/apps/country-stats/src/app/app.component.ts
+++ frontend/apps/country-stats/src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { CsBase } from './shared/cs-base.component';
+import {HttpClient} from "@angular/common/http";
+import {isNullOrUndefined} from "util";
+import { storageConsts } from './consts/consts';
 
 
 @Component({
@@ -10,11 +13,18 @@ import { CsBase } from './shared/cs-base.component';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent extends CsBase implements OnInit {
-  constructor(@Inject(Router) private router: Router) {
+  constructor(@Inject(Router) private router: Router, @Inject(HttpClient) private httpClient: HttpClient) {
     super();
   }
 
   ngOnInit(): void {
+      if(isNullOrUndefined(localStorage.getItem('userLocation.countryCode'))){
+        this.httpClient.get('http://ip-api.com/json').subscribe(result => {
+            localStorage.setItem(storageConsts.countryCode, result['countryCode']);
+            localStorage.setItem(storageConsts.latitude, result['lat']);
+            localStorage.setItem(storageConsts.longitude, result['lon']);
+        });
+    }
     // TODO RANDOM COUNTRY IMAGE
     // https://www.npmjs.com/package/node-shutter-search
     // const shutterSearch = require('shutter-search');
