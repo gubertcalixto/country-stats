7472d93d4420891b7fcd002eb71dda000cf08207
eduardocalixto <eduardogubertcalixto@gmail.com>
2019-06-22 15:55:56 -0300

    Fixed country-banner for production mode

--- frontend/countries-stats/src/app/main/home/country-banner/country-banner.component.ts
+++ frontend/countries-stats/src/app/main/home/country-banner/country-banner.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CsBase } from 'src/app/shared/cs-base.component';
+import { environment } from 'src/environments/environment';
 import { PaisView } from 'src/swagger/swag-proxy';
 import UnsplashSearch from 'unsplash-search';
 
@@ -17,21 +18,29 @@ export class CountryBannerComponent extends CsBase implements OnInit {
   }
 
   ngOnInit() {
-    const accessKey = '4fbf7a1532cb17d0b57ee994e6f40fe19e5387ee115433a7d34b1186c132a240';
-    const provider = new UnsplashSearch(accessKey);
-    provider
-      .searchAll(this.country.nome, 1)
-      .then(data => {
-        if (data.images[0]) {
-          this.countryBackground = new CountryBackground(
-            data.images[0].author.name,
-            data.images[0].urls.full,
-            data.images[0].urls.creditLink
-          );
-        }
-      });
+    if (environment.production) {
+      const accessKey = '4fbf7a1532cb17d0b57ee994e6f40fe19e5387ee115433a7d34b1186c132a240';
+      const provider = new UnsplashSearch(accessKey);
+      provider
+        .searchAll(this.country.nome, 1)
+        .then(data => {
+          if (data.images[0]) {
+            this.countryBackground = new CountryBackground(
+              data.images[0].author.name,
+              data.images[0].urls.full,
+              data.images[0].urls.creditLink
+            );
+          }
+        });
+    }
+    else {
+      this.countryBackground = new CountryBackground(
+        null,
+        '/assets/images/country_default_background.jpg',
+        null
+      );
+    }
   }
-
 }
 
 export class CountryBackground {
