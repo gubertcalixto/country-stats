84afaf80de5f4261b3a7533d8e39197bf7b97585
eduardocalixto <eduardogubertcalixto@gmail.com>
2019-06-22 02:27:19 -0300

    Fixed swag

--- frontend/countries-stats/src/swagger/CgoInterceptor.ts
+++ frontend/countries-stats/src/swagger/CgoInterceptor.ts
@@ -6,7 +6,6 @@ import { Observable } from 'rxjs';
 export class CgoInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    debugger
     const authReq = req.clone({ headers: req.headers.set('Allow-Control-Allow-Origin', '*') });
     return next.handle(authReq);
   }
