d20c0dcea1060f1408b7095c22b9eca7c3c198ff
eduardocalixto <eduardogubertcalixto@gmail.com>
2019-06-22 02:28:11 -0300

    Some small changes

--- frontend/countries-stats/src/app/app.module.ts
+++ frontend/countries-stats/src/app/app.module.ts
@@ -1,9 +1,11 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule, MatIconModule, MatListModule, MatSidenavModule, MatTabsModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { environment } from 'src/environments/environment';
+import { CgoInterceptor } from 'src/swagger/CgoInterceptor';
 import { API_BASE_URL } from 'src/swagger/swag-proxy';
 import { SwaggerModule } from 'src/swagger/swagger-module';
 
@@ -30,7 +32,12 @@ import { LayoutModule } from './layout/layout.module';
     AppRoutingModule
   ],
   providers: [
-    { provide: API_BASE_URL, useValue: environment.backendUrl }
+    { provide: API_BASE_URL, useValue: environment.backendUrl },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: CgoInterceptor,
+      multi: true
+    }
   ],
   bootstrap: [AppComponent]
 })
